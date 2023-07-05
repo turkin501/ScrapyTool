cde8c3530f7ce69d4ea4027a3e70f850fe490550
turkin501 <19522155@gm.uit.edu.vn>
2023-07-06 01:59:40 +0700

    change rule

--- cafe/cafe/spiders/news.py
+++ cafe/cafe/spiders/news.py
@@ -1,24 +1,24 @@
 import scrapy
 from datetime import date
-from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
 
 
 class NewsSpider(CrawlSpider):
     name = "news"
     allowed_domains = ["cafef.vn"]
     start_urls = ["https://cafef.vn"]
-    rules = (
-        # Extract and follow all links!
-        Rule(LinkExtractor(callback='parse_item'), follow=True),
-    )
+    # rules = (
+    #     # Extract and follow all links!
+    #     Rule(LinkExtractor(callback='parse_item'), follow=True),
+    # )
 
     def parse(self, response):
         # Extracting data from the response
-        title = response.css('h3.news-title a::text').get()
-        author = response.css('div.news-publish p:first-child::text').get()
-
-        # Yielding the extracted data
-        yield {
-            'Title': title,
-            'Author': author
-        }
+        for item in response.css("listchungkhoannew"):
+            yield {
+                "title": item.css("knswli-right.h3.a.title::text").get(),
+                "type": item.css("time_cate.p.title::text").get(),
+                # "tags": item.css("div.tags a.tag::text").getall(),
+            }
